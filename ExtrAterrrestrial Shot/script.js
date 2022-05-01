3417378dec19ee60959bd39c3f6db9660257e5d2
Mandee <102609655+MandeeRiz@users.noreply.github.com>
2022-05-01 16:27:50 -0400

    Add files via upload

---
+++ ExtrAterrrestrial Shot/script.js
@@ -0,0 +1,190 @@
+//glossary of ship objects//////////////////////////////////
+let userShip = {
+hull: 20,
+firepower: 5,
+accuracy: .7,
+}
+
+let compShip1 = {
+hull:0,
+firepower: 0,
+accuracy:0,
+}
+
+let compShip2 = {
+hull:0,
+firepower: 0,
+accuracy:0,
+}
+
+let compShip3 = {
+hull:0,
+firepower: 0,
+accuracy:0,
+}
+
+let compShip4 = {
+hull:0,
+firepower: 0,
+accuracy:0,
+}
+
+let compShip5 = {
+hull:0,
+firepower: 0,
+accuracy:0,
+}
+
+let compShip6 = {
+hull:0,
+firepower: 0,
+accuracy:0,
+}
+
+
+//get random hulls for compShips///////////////////////////////////
+
+const randomhull = function(compShip){
+    let randomNum = Math.floor(Math.random()* 4)
+    compShip.hull=randomNum
+    if (compShip.hull === 0){
+        compShip.hull = 4;
+    } else if (compShip.hull === 1){
+        compShip.hull = 5;
+    } else if (compShip.hull === 2){
+        compShip.hull = 6;
+    } else {
+        compShip.hull = 3;
+    }
+}
+randomhull(compShip1);
+randomhull(compShip2);
+randomhull(compShip3);
+randomhull(compShip4);
+randomhull(compShip5);
+randomhull(compShip6);
+
+//get random fire for compShips/////////////////////////////////////
+const randomFire = function(sship){
+   let randomNumb = Math.floor(Math.random()* 3)
+    sship.firepower = randomNumb
+    if(sship.firepower === 0){
+       sship.firepower = 2;
+   } else if (sship.firepower === 1){
+        sship.firepower = 3;
+   } else if (sship.firepower === 2){
+       sship.firepower = 4;
+   } else {
+       console.log("error");
+     }
+}
+
+randomFire(compShip1);
+randomFire(compShip2);
+randomFire(compShip3);
+randomFire(compShip4);
+randomFire(compShip5);
+randomFire(compShip6);
+
+// get random accuracy for compships//////////////////////////////
+const accuracy = function(cship){
+    let ranNum = Math.floor(Math.random()* 3)
+    cship.accuracy = ranNum
+    if(cship.accuracy === 0){
+        cship.accuracy = .6;
+    } else if (cship.accuracy === 1){
+        cship.accuracy = .7;
+    } else if (cship.accuracy === 2){
+        cship.accuracy = .8;
+    }
+}
+
+accuracy(compShip1);
+accuracy(compShip2);
+accuracy(compShip3);
+accuracy(compShip4);
+accuracy(compShip5);
+accuracy(compShip6);
+//==========================================
+ //everything that happens in the game happens in the event listener
+const startButton = document.querySelector(".start")
+let boom = document.querySelector(".boom")
+
+//start of battle
+
+const startBattle = () => {
+   let userMove = prompt("[A]ttack or [R]etreat", "Input A or R");
+    if(userMove === "A"){ 
+        //user attack function runs
+        console.log("You Attacked")
+        shootAnimation();
+        const cIsHit = () => {
+            if(Math.random() < userShip.accuracy){
+                console.log("You hit the alien ship!");
+                compShip1.hull -= userShip.firepower;
+                if(compShip1.hull <= 0){
+                    console.log("You destoyed the alien ship!");
+                    let alien = document.querySelector(".evilShip")
+                    alien.classList.toggle("none")
+                }
+            } else {
+                console.log("You missed!");
+            }
+        }
+        cIsHit();
+    } else if(userMove === "R"){
+        //run game over function "you lose"
+        console.log("you lose")
+        alert("You Lose!")
+    } else{
+        console.log("error, choose A or R.")
+    }
+    //aliens turn
+    let alienMove = alert("Alien ship is attacking!");
+    if(compShip1.hull > 0){ 
+        uIsHit(compShip1);
+        console.log(`Your hull stands at ${userShip.hull}`);
+    } else if (compShip1.hull <= 0){
+        console.log("onto the next alien ship");
+    }
+}
+
+// click start Game to start the game
+startButton.addEventListener("click", startBattle)
+   
+//////////////////////////////////////////////////////////////////////////
+
+//shoot animation
+
+const shootAnimation = () =>{
+boom.classList.toggle("boomtoggle");
+    }
+
+
+//trying to make my shoot animation reset without having the user click again and once more to reset the boom
+ //   let shootAnimationComplete = () =>{
+    //    boom.classList.toggle("boom");
+      //  setTimout(shootAnimationComplete(), 4000);
+    
+
+///////////////////////////////////////////////////////////////////
+
+//calculates if alien ship will be hit by your shot
+const cIsHit = () => {
+    if(Math.random() < userShip.accuracy){
+        console.log("You hit the alien ship!")
+    } else {
+        console.log("You missed!");
+    }
+}
+//calculates only if alien ship 1 will hit the user!
+const uIsHit = (compShip) => {
+    if (Math.random() <= compShip.accuracy){
+        console.log("You have been hit!")
+        userShip.hull -= compShip.firepower
+    } else if (Math.random() > compShip.accuracy){
+        console.log("The alien missed!")
+    } else {
+        console.log("error")
+    }
+}
---
+++ ExtrAterrrestrial Shot/script2.js
@@ -0,0 +1,61 @@
+/*class ship {
+    constructor(user, hull, firepower, accuracy){
+        this.user = user;
+        this.hull = hull;
+        this.firepower = firepower;
+        this.accuracy = accuracy;
+    }
+}
+*/
+/*
+let USSAssembly = {
+
+    retreat: function(){
+        //alows us to retreat
+    }
+}
+//------------------------------------------------------------------------------
+let alienTroop = [
+
+]
+
+//rounds => each player attacks
+
+//after each round you can retreat
+
+//----------------------------------------------------------------------------
+
+const battle = () =>
+//1 user attacks alien ship 1
+//2 if alien survives then they attack
+//3 if you survive again you attack again
+//4 if 
+//playerOne
+
+
+//playerTwo*/
+/*
+
+
+/*const battle =()=>{
+    let playerOneScore = 100
+    let alienScore = 100
+}
+    console.log("battle started!")
+
+    //startGame
+    //Establish a round
+
+let command = prompt("Ready to [a]ttack? or Retreat?")
+
+if(command === "a"){
+    let target=document.querySelector(".evilShip")
+    //signal explosion
+let explosion = document.createElement("img")
+explosion.setAttribute("src", "https://thumbs.gfycat.com/ShyEnlightenedCirriped-max-1mb.gif")
+    //decrease alien hp
+}else{
+        console.log("loser")
+    }
+
+*/
